2cd2cee12647707670808f14a99dbf9533232933
ILostAccessToYcGun <ycgun32@gmail.com>
2025-03-09 13:59:31 -0400

    Basic Menu Done

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -2,16 +2,27 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] GameObject mainPanel;
     [SerializeField] GameObject exitPanel;
+    [SerializeField] GameObject creditsPanel;
 
     public void OnPlay()
     {
-
+        Debug.Log("Play");
+        //Change Scene
     }
 
     public void OnCredits()
     {
+        mainPanel.SetActive(false);
+        creditsPanel.SetActive(true);
 
+    }
+
+    public void OnCreditsBack()
+    {
+        mainPanel.SetActive(true);
+        creditsPanel.SetActive(false);
     }
 
     public void OnExit()
