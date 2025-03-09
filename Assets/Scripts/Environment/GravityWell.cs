6a405a8b4da3c5a038e1454e45c1e18028e52238
ILostAccessToYcGun <ycgun32@gmail.com>
2025-03-09 14:43:10 -0400

    Gravity Wells Done for now

---
+++ Assets/Scripts/Environment/GravityWell.cs
@@ -0,0 +1,23 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GravityWell : MonoBehaviour
+{
+    [SerializeField] float pullForce;
+
+
+    private float GetDistance(Collider2D collision)
+    {
+        return (this.transform.position - collision.transform.position).magnitude;
+    }
+
+    private Vector3 GetPointingTransform(Collider2D collision)
+    {
+        return (this.transform.position - collision.transform.position).normalized;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        collision.attachedRigidbody.AddForce(GetPointingTransform(collision) * GetDistance(collision) * pullForce);
+    }
+}
